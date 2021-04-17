4c9a0ad8e1e2d32dda12fe2381ebadbb3379e67e
dmoore <darren.m95@icloud.com>
2021-04-17 19:19:45 +0100

    Add tasks to state in App.tsx and pass as prop to Tasks.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
+interface Task {
+  id: number;
+  text: string;
+  day: string;
+  reminder: boolean;
+}
+
+const initialTasks: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'Feb 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'Feb 5th at 2:30pm',
+    reminder: false,
+  },
+];
+
 const App = (): JSX.Element => {
+  const [tasks, setTasks] = useState(initialTasks);
+
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,25 +1,13 @@
-const tasks: { id: number; text: string; day: string; reminder: boolean }[] = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: false,
-  },
-];
+interface ITasksProps {
+  tasks: {
+    id: number;
+    text: string;
+    day: string;
+    reminder: boolean;
+  }[];
+}
 
-const Tasks = (): JSX.Element => {
+const Tasks = ({ tasks }: ITasksProps): JSX.Element => {
   return (
     <>
       {tasks.map((task) => (
