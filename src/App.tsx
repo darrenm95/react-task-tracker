69b67c14d13b6e5476546e1e32c68000e1a56159
dmoore <darren.m95@icloud.com>
2021-04-17 21:26:26 +0100

    Add AddTask component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import initialTasks from './tasksData.json';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import initialTasks from './tasksData.json';
+import AddTask from './components/AddTask';
 
 const App = (): JSX.Element => {
   const [tasks, setTasks] = useState(initialTasks);
@@ -23,6 +24,7 @@ const App = (): JSX.Element => {
   return (
     <div className='container'>
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
